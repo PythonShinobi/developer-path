a8a7372fbbc50d5fa9d4bdddd796d5e62383ab60
PythonShinobi <pythonshinobi@gmail.com>
2024-07-14 15:34:36 +0300

    Add route path for the backend page.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import FrontendDeveloperPage from './frontend-developer/Frontend';
+import BackendDeveloperPage from './backend-developer/Backend';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/frontend-developer' element={<FrontendDeveloperPage />}/>
+        <Route path='/backend-developer' element={<BackendDeveloperPage />}/>
       </Routes>
     </BrowserRouter>
   );
