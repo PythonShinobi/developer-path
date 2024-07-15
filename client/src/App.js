256f44ad63c813840c2235f83842149e7fbdadb5
PythonShinobi <pythonshinobi@gmail.com>
2024-07-15 08:03:44 +0300

    Add route path for data-scientist page.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import FrontendDeveloperPage from './frontend-developer/Frontend';
 import BackendDeveloperPage from './backend-developer/Backend';
 import APIDeveloperPage from './api-developer/Api';
 import DatabaseDeveloperPage from './database-developer/Database';
+import DataScientistPage from './data-scientist/DataScientist';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path='/backend-developer' element={<BackendDeveloperPage />}/>
         <Route path='/api-developer' element={<APIDeveloperPage />}/>
         <Route path='/database-developer' element={<DatabaseDeveloperPage />}/>
+        <Route path='/data-scientist' element={<DataScientistPage />}/>
       </Routes>
     </BrowserRouter>
   );
